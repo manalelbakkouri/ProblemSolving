a62f015608d341782b293d79b10c87b9a51a53ac
Manal El-bakkouri <156050054+manalelbakkouri@users.noreply.github.com>
2025-08-03 15:06:14 +0100

    Time: 0 ms (100.00%) | Memory: 12.9 MB (97.84%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,7 +5,7 @@ class Solution(object):
         
         for i , v in enumerate(nums):
             if target-v in hash_map :
-               return i , hash_map[target-v]
+               return [i , hash_map[target-v]]
             else :
                hash_map[v]=i
 
